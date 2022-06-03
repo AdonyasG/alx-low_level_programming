041c05d1f9f9379c98d5b2fc27de633ebe6bef7b
AdonyasG <adonyas2000@gmail.com>
2022-06-03 04:11:51 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return:always return 0
+ *
+ */
+int main(void)
+{
+	char lc = 'a';
+	for (lc = 'a'; lc<='z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
