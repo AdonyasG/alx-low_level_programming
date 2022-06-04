84fb9077a9a7f20ac49432f277e7734571dc5fe1
AdonyasG <adonyas2000@gmail.com>
2022-06-04 03:48:33 -0700

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return:always return 0
+ *
+ */
+int main(void)
+{
+	int i;
+	int n;
+	int j;
+
+	for (i = 10; i <= 19; i++)
+	{
+	for (n = 10; n <= 19; n++)
+	{
+	for (j = 10; j <= 19; j++)
+	{
+	if ((j % 10) > (n % 10) && (n % 10) > (i % 10))
+	{
+		putchar((i % 10) + '0');
+		putchar((n % 10) + '0');
+		putchar((j % 10) + '0');
+	if (i != 18 || n != 19 || j != 20)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
