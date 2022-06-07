4e6645a59aebba60e0df261454fd205b330c213e
AdonyasG <adonyas2000@gmail.com>
2022-06-07 04:56:49 -0700

    tak 1 completed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ *
+ */
+void print_alphabet(void)
+{
+	char A = 'a';
+
+	for (; A <= 'z'; A++)
+	{
+	_putchar(A);
+	}
+	_putchar('\n');
+}
