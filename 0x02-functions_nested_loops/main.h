befcf68bfde1c95d95b162247142b8c69dc1bbfa
AdonyasG <adonyas2000@gmail.com>
2022-06-09 00:11:08 -0700

    task 8

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -46,4 +46,8 @@ int _abs(int);
  * Return: 0
  */
 int print_last_digit(int);
+/**
+ * void jack_bauer(void) - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ */
+void jack_bauer(void);
 #endif
