155cd742de2df27db0f7c785f0d99887271faf74
AdonyasG <adonyas2000@gmail.com>
2022-06-13 17:34:32 +0000

    task 5 added

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: character to compute
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		i--;
+	}
+}
