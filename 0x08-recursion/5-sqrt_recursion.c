17cde2ae182632c69d917c179dfc03d0ecbe0a7f
AdonyasG <adonyas2000@gmail.com>
2022-06-22 05:55:11 -0700

    task 5 added

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _sq_help - helper
+ * @s: value to compute
+ * @n:value to compute
+ * Return: output
+ */
+int _sq_help(int s, int n)
+{
+	if (s * s == n)
+	{
+		return (s);
+	}
+	else if(s * s > n)
+	{
+		return (-1);
+	}
+	return (_sq_help(n, s + 1));
+}
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: value to compute
+ * Return: value of root
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sq_help(n, 0));
+}
