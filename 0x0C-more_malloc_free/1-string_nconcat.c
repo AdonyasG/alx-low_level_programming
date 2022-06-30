8520094046ef353b40a9178068e6f3c83971b344
AdonyasG <adonyas2000@gmail.com>
2022-06-30 03:14:55 -0700

    task 1 added

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concats strings
+ * @s1: string one
+ * @s2: string two
+ * @n: n amount of bytes
+ * Return: return a char val
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k;
+	char *c;
+
+	if (s1 == NULL)
+		return ("");
+	if (s2 == NULL)
+		return ("");
+	j = 0;
+	k = 0;
+	while (s1[j] != '\0')
+		j++;
+	while (s2[k] != '\0')
+		k++;
+	if (n >= k)
+		n = k;
+	c = malloc(sizeof(char) * n + j + 1);
+	if (c == NULL)
+		return (NULL);
+	for (i = 0; i < j; i++)
+		c[i] = s1[i];
+	for (i = 0; i < n; i++)
+	       c[i + j] = s2[i];
+	s1[i + j] = '\0';
+	return (c);
+}
