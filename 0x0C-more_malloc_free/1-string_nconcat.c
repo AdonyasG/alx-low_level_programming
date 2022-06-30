f4fdf611ed8d7d2808114920422551609fde256f
AdonyasG <adonyas2000@gmail.com>
2022-06-30 10:35:59 -0700

    task 1 added

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,6 +18,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return ("");
 	j = 0;
 	k = 0;
+	i = 0;
 	while (s1[j] != '\0')
 		j++;
 	while (s2[k] != '\0')
@@ -27,10 +28,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	c = malloc(sizeof(char) * n + j + 1);
 	if (c == NULL)
 		return (NULL);
-	for (i = 0; i < j; i++)
+	for (; i < j; i++)
+	{
 		c[i] = s1[i];
-	for (i = 0; i < n; i++)
-	       c[i + j] = s2[i];
+	}
+	for (; i < n; i++)
+	{
+		c[i + j] = s2[i];
+	}
 	c[i + j] = '\0';
 	return (c);
 }
