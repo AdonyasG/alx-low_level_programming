5543c2797e3ec4f3637dba0a1790a5de0fb15b42
AdonyasG <adonyas2000@gmail.com>
2022-06-30 10:47:11 -0700

    task 1 fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *c;
 
 	if (s1 == NULL)
-		return ("");
+		s1 = "";
 	if (s2 == NULL)
-		return ("");
+		s2 = "";
 	j = 0;
 	k = 0;
 	while (s1[j] != '\0')
