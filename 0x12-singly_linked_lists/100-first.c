a8186d6004512d193e6dd6e5d9b80ee727b75ab5
AdonyasG <adonyas2000@gmail.com>
2022-07-14 11:50:02 -0700

    advanced task and updated header

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * tortoise - prints text
+ *
+ */
+void tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
