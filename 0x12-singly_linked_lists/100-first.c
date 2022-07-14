3195e1119644cbebe05ac537d7a774571774a040
AdonyasG <adonyas2000@gmail.com>
2022-07-14 12:00:26 -0700

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
  * tortoise - prints text
- *
+ * Return: void
  */
-void tortoise(void)
+void __attribute__ ((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
