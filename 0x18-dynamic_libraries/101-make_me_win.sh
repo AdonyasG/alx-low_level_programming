9258deb37ffb8d73df7facc4d12fc2cf56b70645
AdonyasG <adonyas2000@gmail.com>
2022-09-05 09:30:18 -0400

    advanced task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl --create-dirs -O --output-dir /run/rand.so https://AdonyasG/alx-low_level_programming/master/0x18-dynamic_libraries/rand.so
+wget -P /run/rand.so https://AdonyasG/alx-low_level_programming/master/0x18-dynamic_libraries/rand.so
 export LD_PRELOAD=/run/rand.so
