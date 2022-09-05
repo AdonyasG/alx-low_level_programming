1166050e2d64797a83a6abf5c511e90abcfc97ba
AdonyasG <adonyas2000@gmail.com>
2022-09-05 14:38:11 -0400

    trial with curl

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -O /../ https://github.com/AdonyasG/alx-low_level_programming/raw/master/0x18-dynamic_libraries/rand.so
-export LD_PRELOAD=/$PWD/rand.so
+export LD_PRELOAD="/$PWD/rand.so"
